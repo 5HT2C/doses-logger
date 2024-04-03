720b919b1f35350f147b6ad5f47c3224c8ad8774
5HT2 <git@frogg.ie>
2024-04-03 15:05:18 -0700

    revert: "date-first-tried: Fix breaking on drug names containing `,`"
    This reverts commit 13b0ac8131533fc4bc0ca35e6edd135fa8f51efb.

--- date-first-tried.sh
+++ date-first-tried.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./doses-logger -n -1 -j | jq '.[] | .drug + ":" + .date' -r | awk '{ split($0,a,":"); { if (d[a[1]] == "") { d[a[1]]=a[2]; printf "%s %s\n", d[a[1]], a[1] } }}'
+./doses-logger -n -1 -j | jq '.[] | .drug + "," + .date' -r | awk '{ split($0,a,","); { if (d[a[1]] == "") { d[a[1]]=a[2]; printf "%s %s\n", d[a[1]], a[1] } }}'
